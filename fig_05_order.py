3095f9966762bc8d3b14c1f77fc0b7991836c65e
Tobias Erhardt <tobias.erhardt@gmail.com>
2019-04-05 09:56:44 +0200

    Fix figure 5 axis labels

--- fig_05_order.py
+++ fig_05_order.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import joblib as jl
+from code.plotting import parlabels
 
 traces = jl.load('ramp_fits/traces/NGRIP.gz')
 
@@ -14,7 +15,7 @@ for i, event in enumerate(traces.coords['event'].values):
     t0_order = np.argsort(t0, axis=1)
     f = lambda x: np.bincount(x, minlength=4)
     order_freq[i] = np.array(list(map(f, t0_order.values.T))) / 12000
-    
+
 mean_order = np.mean(order_freq, axis=0)
 print(pd.DataFrame(mean_order.T, index=traces.coords['param'].values))
 
@@ -23,7 +24,8 @@ img = ax.imshow(mean_order.T, cmap=plt.cm.Blues)
 fig.colorbar(img, label='Average probability')
 ax.set_xlabel('Order at start of transition')
 ax.set_yticks(np.arange(4))
-ax.set_yticklabels(traces.coords['param'].values)
+labels = [parlabels[p] for p in traces.coords['param'].values]
+ax.set_yticklabels(labels)
 ax.set_xticks(np.arange(4))
 ax.set_xticklabels(np.arange(4) + 1)
 
